c6d2b0b23f3a486f69d550f2df96eaccdb46c1cc
Rutok <n.boste@gmail.com>
2017-02-23 06:16:38 +0100

    New projection : parallel ! :-)

--- srcs/camera.c
+++ srcs/camera.c
@@ -6,7 +6,7 @@
 /*   By: nboste <nboste@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/24 23:09:07 by nboste            #+#    #+#             */
-/*   Updated: 2017/02/20 02:08:48 by nboste           ###   ########.fr       */
+/*   Updated: 2017/02/23 04:49:02 by nboste           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,37 +39,44 @@ void		init_camera(t_env *env, double rad_v_field, t_camera *camera)
 
 void		camera_project_vertex(t_3dvertex *v, t_2dpair *c_view, t_camera *camera)
 {
-
-	if (v->z > 0)
+	if (camera->projection == perspective)
 	{
-		c_view->x = (v->x * camera->d) / (v->z * camera->ratio);
-		c_view->y = (v->y * camera->d) / v->z;
-	}
-	else if (v->z < 0)
-	{
-		c_view->x = (v->x * camera->d) / (-v->z * camera->ratio);
-		c_view->y = (v->y * camera->d) / (-v->z);
+		if (v->z > 0)
+		{
+			c_view->x = (v->x * camera->d) / (v->z * camera->ratio);
+			c_view->y = (v->y * camera->d) / v->z;
+		}
+		else if (v->z < 0)
+		{
+			c_view->x = (v->x * camera->d) / (-v->z * camera->ratio);
+			c_view->y = (v->y * camera->d) / (-v->z);
+		}
+		else
+		{
+			c_view->x = (v->x * camera->d) /(camera->ratio);
+			c_view->y = (v->y * camera->d);
+		}
+	c_view->x = round(((c_view->x + camera->ratio) * camera->size.x)  / (2 * camera->ratio));
+	c_view->y = round(((c_view->y + 1) * camera->size.y)  / (2));
 	}
 	else
 	{
-		c_view->x = (v->x * camera->d) /(camera->ratio);
-		c_view->y = (v->y * camera->d);
+			c_view->x = (v->x * camera->ratio) + (camera->size.x / 2);
+			c_view->y = v->y + (camera->size.y / 2);
 	}
-	c_view->x = round(((c_view->x + camera->ratio) * camera->size.x)  / (2 * camera->ratio));
-	c_view->y = round(((c_view->y + 1) * camera->size.y)  / (2));
 }
 
 void		to_camera_space(t_3dvertex* v, t_3dvertex* c_space, t_camera *camera)
 {
 	c_space->x = (v->x - camera->pos.x) * camera->u.x
-				+ (v->y - camera->pos.y) * camera->u.y
-				+ (v->z - camera->pos.z) * camera->u.z;
+		+ (v->y - camera->pos.y) * camera->u.y
+		+ (v->z - camera->pos.z) * camera->u.z;
 	c_space->y = (v->x - camera->pos.x) * camera->v.x
-				+ (v->y - camera->pos.y) * camera->v.y
-				+ (v->z - camera->pos.z) * camera->v.z;
+		+ (v->y - camera->pos.y) * camera->v.y
+		+ (v->z - camera->pos.z) * camera->v.z;
 	c_space->z = (v->x - camera->pos.x) * camera->n.x
-				+ (v->y - camera->pos.y) * camera->n.y
-				+ (v->z - camera->pos.z)* camera->n.z;
+		+ (v->y - camera->pos.y) * camera->n.y
+		+ (v->z - camera->pos.z)* camera->n.z;
 }
 
 void		reset_camera_pixels(t_camera *camera)
